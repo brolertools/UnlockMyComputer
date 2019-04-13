e4ab9722a100b1b6c2562e54f943ffa7534310d8
Kingtous <kingtous@qq.com>
2019-04-13 19:22:20 +0800

    更新Linux的WiFi、蓝牙客户端

--- Linux-Release/README.md
+++ Linux-Release/README.md
@@ -8,9 +8,11 @@
     - pexpect
     - re
     - subprocess
+    - socket
+    - threading
 - 使用loginctl解锁
 - 使用
 - 可注册为systemd组件
-- 实时监听蓝牙
+- 实时监听WLAN、蓝牙接口
 
 ** PS:blue.py可以在root和普通用户下执行，普通用户需要更改rfcomm的执行权限(Linux下普通用户默认没有执行此可执行文件的权限)，使用```sudo chmod u+x /usr/bin/rfcomm```即可. **
--- Linux-Release/common.py
+++ Linux-Release/common.py
@@ -1,23 +1,37 @@
-import bluetooth
-import json
 import subprocess
 import re
 import os
 import pexpect
+import json
 import time
 
+Debug=False
 
-Debug=True
-uuid = "4E5877C0-8297-4AAE-B7BD-73A8CBC1EDAF"
 
-def log(text):
-    if Debug:
-        print(text)
+def getData(data):
+    contentDict = json.loads(data)
+    username = contentDict['username']
+    passwd = contentDict['passwd']
+    session_id = getSession(username)
+    return username,passwd,session_id
 
 
 def decrypt(passwd):
     # TODO 解密接口
     return passwd
+
+
+def checkLockScreen():
+    '''
+    :return: True for locked & False for unlocked
+    '''
+    # TODO 检测锁屏状态
+    return True
+
+
+def log(text):
+    if Debug:
+        print(text)
 
 
 def getSession(username):
@@ -53,7 +67,6 @@ def unlockStatus(session_id):
     ret = subprocess.check_output(['/bin/loginctl', 'show-session',session_id]).decode('utf-8').strip()
     return re.search('Active=[^\s]+',ret).group().split('=')[1]
 
-
 def unlock(session_id):
     log('执行解锁指令')
     subprocess.Popen(['/bin/loginctl','unlock-session',session_id],stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
@@ -67,50 +80,3 @@ def certificate(passwd,session_id):
     text=str(pro.read().strip().strip(),encoding='utf8')
     if(text=='Y'):
         unlock(session_id)
-
-def checkLockScreen():
-    '''
-    :return: True for locked & False for unlocked
-    '''
-    # TODO 检测锁屏状态
-    return True
-
-
-if __name__ == "__main__":
-    while True:
-        if checkLockScreen():
-            server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-            server_sock.bind(("", bluetooth.PORT_ANY))
-            server_sock.listen(1)
-
-            port = server_sock.getsockname()[1]
-
-            bluetooth.advertise_service(server_sock, "Unlock", service_id=uuid)
-            print("Waiting for connection on RFCOMM channel %d" % port)
-            client_sock, client_info = server_sock.accept()
-            print(client_info)
-
-            data=None
-            try:
-                while True:
-                    data = client_sock.recv(1024)
-                    if len(data) == 0:
-                        break
-                    print("received [%s]" % data)
-            except IOError:
-                pass
-            try:
-                data=str(data,encoding='utf8')
-            except:
-                exit(-1)
-            contentDict=json.loads(data)
-            username=contentDict['username']
-            passwd=contentDict['passwd']
-
-            session_id=getSession(username)
-            if session_id!=None and unlockStatus(session_id)=='yes':
-                certificate(passwd,session_id)
-                client_sock.close()
-                server_sock.close()
-        else:
-            time.sleep(3)
