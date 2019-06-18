e20ed70d06ca15110db1b83a6bd8ea20173300aa
Kingtous <kingtous@qq.com>
2019-06-18 19:52:47 +0800

    6.18 1.完善https代理功能 2.修bug

---
+++ Linux-Release/wake_on_lan_pi.py
@@ -0,0 +1,137 @@
+# udp_gb_client.py
+'''树莓派设置py'''
+
+import socket
+import json
+import os
+import threading
+import ssl
+import binascii
+import re
+import time
+from Config import *
+
+PORT = 8970
+
+MAC_ADDR = None
+IP = None
+PC_NAME = None
+
+
+def loadConfig():
+    global MAC_ADDR, IP, PC_NAME
+    if not os.path.exists('config.ini'):
+        return False
+    f = open('config.ini', 'r')
+    PC_NAME = f.readline().strip()
+    MAC_ADDR = f.readline().strip()
+    IP = f.readline().strip()
+
+    if MAC_ADDR=="":
+        return False
+    print('载入配置文件成功')
+    return True
+
+
+def startBind():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    s.bind(('', PORT))
+    print('Listening for broadcast at ', s.getsockname())
+
+    while True:
+        data, address = s.recvfrom(65535)
+        data = json.loads(data)
+        if data.get('macaddr', -1) != -1 and data.get('pcname', -1) != -1:
+            f = open('config.ini', 'w')
+            f.write(data['pcname'] + '\n' + data['macaddr'] + '\n' + address[0])
+            f.close()
+            return
+
+
+def create_magic_packet(mac):
+    pro_data = 'FF' * 6 + mac * 16
+    send_data = binascii.unhexlify(pro_data)
+    return send_data
+
+
+def format_mac(mac):
+    mac_re = re.compile(r'''
+                      (^([0-9A-F]{1,2}[-]){5}([0-9A-F]{1,2})$
+                      |^([0-9A-F]{1,2}[:]){5}([0-9A-F]{1,2})$
+                      |^([0-9A-F]{1,2}[.]){5}([0-9A-F]{1,2})$
+                      )''', re.VERBOSE | re.IGNORECASE)
+    # print(re.match(mac_re, mac))
+    if re.match(mac_re, mac):
+        if mac.count(':') == 5 or mac.count('-') == 5 or mac.count('.'):
+            sep = mac[2]
+            mac_fm = mac.replace(sep, '')
+            return mac_fm
+    else:
+        raise ValueError('Incorrect MAC format')
+
+
+class Reader(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self)
+        self.client = client
+
+    def run(self):
+        global MAC_ADDR, IP, PC_NAME
+        while True:
+            data = self.client.recv(BUFSIZE)
+            if data:
+                broadcast_address = '255.255.255.255'
+                port = 9
+                # ======================================
+                send_data = create_magic_packet(MAC_ADDR)
+
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+                for i in range(5):
+                    s.sendto(send_data, (broadcast_address, port))
+                    s.sendto(send_data, (IP, port))
+                s.close()
+                print('发送成功')
+            else:
+                break
+
+
+# a listen thread, listen remote connect
+# when a remote machine request to connect, it will create a read thread to handle
+class Connector(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.port = port
+        # SSL
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ssl_sock = ssl.wrap_socket(self.socket, ca_certs="cacert.pem", cert_reqs=ssl.CERT_REQUIRED)
+
+
+    def run(self):
+        print("Connector started")
+        while True:
+            if not self.ssl_sock._connected:
+                try:
+                    self.ssl_sock.connect((NAT_SERVER, self.port))
+                    print('连接成功')
+                    Reader(self.ssl_sock).start()
+                except ConnectionRefusedError:
+                    # 重新初始化
+                    self.__init__(self.port)
+                    time.sleep(3)
+                    print('3s后尝试连接NAT公网代理服务器')
+                    continue
+                Reader(self.ssl_sock).start()
+            if self.ssl_sock._closed:
+                print('重新连接')
+                self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.ssl_sock = ssl.wrap_socket(self.socket, ca_certs="cacert.pem", cert_reqs=ssl.CERT_REQUIRED)
+
+
+if __name__ == '__main__':
+    if not loadConfig():
+        startBind()
+    else:
+        Connector(NAT_WAKE_ON_LAN_PORT).start()
--- app/src/main/java/com/kingtous/remotefingerunlock/MenuTool/RecordPopupMenuTool.java
+++ app/src/main/java/com/kingtous/remotefingerunlock/MenuTool/RecordPopupMenuTool.java
@@ -2,21 +2,32 @@ package com.kingtous.remotefingerunlock.MenuTool;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Looper;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.PopupMenu;
 
+import com.google.gson.JsonObject;
 import com.kingtous.remotefingerunlock.Common.ToastMessageTool;
 import com.kingtous.remotefingerunlock.DataStoreTool.RecordData;
 import com.kingtous.remotefingerunlock.R;
+import com.kingtous.remotefingerunlock.Security.SSLSecurityClient;
 import com.kingtous.remotefingerunlock.WLANConnectTool.WLANConnect;
 import com.kingtous.remotefingerunlock.WLANConnectTool.WLANConnectActivity;
+import com.kingtous.remotefingerunlock.WLANConnectTool.WLANDeviceData;
 import com.stealthcopter.networktools.WakeOnLan;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.nio.charset.StandardCharsets;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -32,31 +43,34 @@ public class RecordPopupMenuTool {
 
     public static void wakeOnLAN(final Context context, final RecordData data){
         if (data.getType().equals("WLAN")){
+            // 获取内网flag
+            int flags=0;
+            SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(context);
+            if (!preferences.getString(context.getString(R.string.connect_mode),"0").equals("0")) {
+                flags = 1;
+            }
+
             if (data.getMac()!=null && data.getIp()!=null) {
                 if (WLANConnectActivity.isWifiConnected(context)) {
                     // 此时肯定是有MAC地址的，无MAC地址已经在UI界面就停止了
 //                    final RecordData data1 = WLANConnect.checkIpEqualMac(context, data);
                     if (data != null && data.getIp() != null && data.getMac() != null) {
-                        new Thread(new wake_on_lan(context,data)).start();
+                        new Thread(new wake_on_lan(context,data,flags)).start();
                     }
                     else {
                         ToastMessageTool.ttl(context,"数据不符且无法自动调整，放弃发送");
                     }
                 } else {
+                    final int finalFlags = flags;
                     new AlertDialog.Builder(context)
-                            .setMessage("检测到非WLAN网络，请问接下来...")
-                            .setPositiveButton("直接连接", new DialogInterface.OnClickListener() {
+                            .setMessage("检测到非WLAN网络，意味着无法访问内网，需要借助内网代理，是否继续...")
+                            .setPositiveButton("连接", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
-                                    new Thread(new wake_on_lan(context,data)).start();
+                                    new Thread(new wake_on_lan(context,data, finalFlags)).start();
                                 }
                             })
-                            .setNegativeButton("内网连接", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    //TODO 内网接口
-                                }
-                            }).show();
+                            .setNegativeButton("取消", null).show();
                 }
             }
         }
@@ -71,18 +85,47 @@ public class RecordPopupMenuTool {
 
         Context context;
         RecordData data;
+        int flags;
 
-        wake_on_lan(Context context,RecordData data){
+        wake_on_lan(Context context,RecordData data,int flags){
             this.context=context;
             this.data=data;
+            this.flags=flags;
         }
 
         @Override
         public void run() {
             Looper.prepare();
+            int result=-1;
             try {
-                WakeOnLan.sendWakeOnLan(data.getIp(), data.getMac());
-                ToastMessageTool.ttl(context, "已发送Wake On LAN数据包，请自行检查远程端是否接收到");
+                if (flags==0) {
+                    WakeOnLan.sendWakeOnLan(data.getIp(), data.getMac());
+                    //全网广播
+                    WakeOnLan.sendWakeOnLan(InetAddress.getByName("255.255.255.255"), data.getMac(), 9, 10000, 5);
+                    result = 0;
+                }
+                else {
+                    // 内网模式
+                    try {
+                        Socket socket=SSLSecurityClient.CreateSocket(context,context.getString(R.string.nat_server), WLANDeviceData.nat_wake_on_lan);
+                        if (socket!=null){
+                            OutputStream stream=socket.getOutputStream();
+                            if (stream!=null){
+                                JsonObject object=new JsonObject();
+                                object.addProperty("mac",data.getMac());
+                                object.addProperty("ip",data.getIp());
+                                stream.write(object.toString().getBytes(StandardCharsets.UTF_8));
+                                stream.close();
+                                socket.close();
+                                result = 0;
+                            }
+                        }
+                    }catch (SocketException e){
+                        ToastMessageTool.ttl(context, "未发送，请检查与服务器的连接");
+                    }
+                }
+                if (result==0)
+                    ToastMessageTool.ttl(context, "已发送Wake On LAN数据包，请自行检查远程端是否接收到");
             } catch (IOException e) {
                 ToastMessageTool.ttl(context, e.getMessage());
             }
